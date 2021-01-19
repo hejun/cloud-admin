2551aac51f67802bce7d891902e8bdfbd8bd0d43
HeJun <1255524587@qq.com>
2021-01-19 16:54:59 +0800

    引入Store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {createApp} from 'vue'
 import router from './router'
+import {key, store} from "./store"
 import App from './App.vue'
 import './assets/style/index.less'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store, key).mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import {InjectionKey} from 'vue'
+import {createStore, Store, useStore as baseUseStore} from 'vuex'
+
+export interface State {
+}
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+})
+
+export function useStore(): Store<State> {
+  return baseUseStore(key)
+}
