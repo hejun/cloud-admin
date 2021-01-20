45083184e0a3668f92e62b6ccdde199adbb4c7b8
HeJun <1255524587@qq.com>
2021-01-20 11:27:58 +0800

    Vuex刷新信息恢复

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,17 @@ export default defineComponent({
   setup() {
     const router = useRouter()
     const store = useStore()
+
+    //处理 store 浏览器刷新丢失问题
+    const STORE_KEY = "KEY_CLOUD_ADMIN_STORE"
+    if (window.sessionStorage.getItem(STORE_KEY)) {
+      store.replaceState(Object.assign({}, store.state, JSON.parse(window.sessionStorage.getItem(STORE_KEY))))
+    }
+    window.addEventListener('beforeunload', () => {
+      window.sessionStorage.setItem(STORE_KEY, JSON.stringify(store.state))
+    })
+
+    //处理登录状态
     const authorized = store.getters.authorized
     watchEffect(() => authorized.value ? router.replace("/") : router.replace("/login"))
   }
