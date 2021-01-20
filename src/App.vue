31b353f5a9dd8ee9f0cb68ab989e73f3d64a8448
HeJun <1255524587@qq.com>
2021-01-20 11:32:00 +0800

    Vuex刷新信息恢复

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,11 @@ export default defineComponent({
 
     //处理 store 浏览器刷新丢失问题
     const STORE_KEY = "KEY_CLOUD_ADMIN_STORE"
-    if (window.sessionStorage.getItem(STORE_KEY)) {
-      store.replaceState(Object.assign({}, store.state, JSON.parse(window.sessionStorage.getItem(STORE_KEY))))
+    if (<any>window.sessionStorage.getItem(STORE_KEY)) {
+      store.replaceState(Object.assign({}, store.state, JSON.parse(<any>window.sessionStorage.getItem(STORE_KEY))))
     }
-    window.addEventListener('beforeunload', () => {
-      window.sessionStorage.setItem(STORE_KEY, JSON.stringify(store.state))
+    <any>window.addEventListener('beforeunload', () => {
+      <any>window.sessionStorage.setItem(STORE_KEY, JSON.stringify(store.state))
     })
 
     //处理登录状态
