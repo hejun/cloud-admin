9f919570c1e656fc430f9ba58368610f680df028
HeJun <1255524587@qq.com>
2021-01-20 11:12:47 +0800

    监听登录状态,实时切换页面

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,17 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, watchEffect} from 'vue'
+import {useRouter} from "vue-router"
+import {useStore} from "./store"
 
 export default defineComponent({
   name: 'App',
   setup() {
+    const router = useRouter()
+    const store = useStore()
+    const authorized = store.getters.authorized
+    watchEffect(() => authorized.value ? router.replace("/") : router.replace("/login"))
   }
 })
 </script>
